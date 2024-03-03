2c4e4e45373c09a4127efb45e3e6dab644fd8cef
umaidashi <umaidashi18@gmail.com>
2024-03-03 22:18:40 +0900

    init

---
+++ gofr/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "gofr.dev/pkg/gofr"
+)
+
+func main(){
+  app := gofr.New()
+
+  app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
+
+    return "Hello world", nil
+
+  })
+}
